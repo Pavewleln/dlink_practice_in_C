b4b0260396e99e511ea1d1752db81f25e932b1e6
pavel <kulikovps2004@gmail.com>
2023-07-18 19:42:41 +0300

    remove extra files and update code in main.c

---
+++ practice_2/default.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+	printf("Name\tAge\tCity\t\n");
+	printf("John\t25\tNew York\t\n");
+	printf("Alice\t30\tLos Angeles\t\n");
+	printf("Michael\t35\tChicago\t\n");
+	return 0;
+}
