d550b9f69b0fc1937e168b79a31d8c24f5b9c617
pavel <kulikovps2004@gmail.com>
2023-08-20 17:02:04 +0300

    Changed makefile. It can create static libraries

--- practice_3/include/plugins.h
+++ practice_3/include/plugins.h
@@ -5,6 +5,6 @@
 void remove_tags(const char *html);
 void only_text_plugin(const char *html, size_t *html_size, FILE *file);
 void headers_plugin(const char *html, size_t *html_size, FILE *file);
-void links_plugin(cconst char *html, size_t *html_size, FILE *file);
+void links_plugin(const char *html, size_t *html_size, FILE *file);
 
 #endif
